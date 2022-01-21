fc0ae69ce6c7ccc2447d5085dbf634f172daf9fe
kulas115 <kulas115@gmail.com>
2022-01-21 16:33:29 +0100

    Add first base classes for protytype

---
+++ 03-prototype/game_window.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class GameWindow < Gosu::Window
+  attr_accessor :state
+
+  def initialize
+    super(800, 600, false)
+  end
+
+  def update
+    @state.update
+  end
+
+  def draw
+    @state.draw
+  end
+
+  def need_redraw?
+    @state.needs_redraw?
+  end
+
+  def button_down(id)
+    @state.button_down(id)
+  end
+end
