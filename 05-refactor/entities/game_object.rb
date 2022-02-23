c16f042f80e54b5cd5b74d5260b713f52c9c7421
kulas115 <kulas115@gmail.com>
2022-02-23 19:31:18 +0100

    Start refactoring using component and pool_object patterns

---
+++ 05-refactor/entities/game_object.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class GameObject
+  def initialize(object_pool)
+    @components = []
+    @object_pool = object_pool
+    @object_pool.objects << self
+  end
+
+  attr_reader :components
+
+  def update
+    @components.map(&:update)
+  end
+
+  def draw(viewport)
+    @components.each { |c| c.draw(viewport) }
+  end
+
+  def removable?
+    @removable
+  end
+
+  def mark_for_removal
+    @removable = true
+  end
+
+  protected
+
+  attr_reader :object_pool
+end
