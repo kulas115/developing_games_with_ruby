8b17ea9f1c89dc5ebfe6eff6b953876ed2b855bd
kulas115 <kulas115@gmail.com>
2022-02-27 19:26:40 +0100

    Refactor PlayState

--- 05-refactor/entities/components/tank_graphics.rb
+++ 05-refactor/entities/components/tank_graphics.rb
@@ -1,15 +1,17 @@
+# frozen_string_literal: true
+
 class TankGraphics < Component
   def initialize(game_object)
     super(game_object)
-    @body= units.frame('tank1_body.png')
+    @body = units.frame('tank1_body.png')
     @shadow = units.frame('tank1_body_shadow.png')
     @gun = units.frame('tank1_dualgun.png')
   end
 
-  def draw(viewport)
-    @shadow.draw_rot(x - 1, y -1, 0, object.direction)
-    @body.draw_rot(x,y,1,object.direction)
-    @gun.draw_rot(x,y,2,object.gun_angle)
+  def draw(_viewport)
+    @shadow.draw_rot(x - 1, y - 1, 0, object.direction)
+    @body.draw_rot(x, y, 1, object.direction)
+    @gun.draw_rot(x, y, 2, object.gun_angle)
   end
 
   private
