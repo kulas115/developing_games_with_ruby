4b4a78b9b6801879bfe793e69aad8ac8ba12aa8b
kulas115 <kulas115@gmail.com>
2022-03-03 19:31:29 +0100

    Add bullet collisions with other tanks

--- 06-physics/entities/bullet.rb
+++ 06-physics/entities/bullet.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Bullet < GameObject
-  attr_accessor :x, :y, :target_x, :target_y, :speed, :fired_at
+  attr_accessor :x, :y, :target_x, :target_y, :source, :speed, :fired_at
 
   def initialize(object_pool, source_x, source_y, target_x, target_y)
     super(object_pool)
@@ -9,7 +9,7 @@ class Bullet < GameObject
     @y = source_y
     @target_x = target_x
     @target_y = target_y
-    BulletPhysics.new(self)
+    BulletPhysics.new(self, object_pool)
     BulletGraphics.new(self)
     BulletSounds.play
   end
@@ -24,6 +24,7 @@ class Bullet < GameObject
   end
 
   def fire(speed)
+    @source = source
     @speed = speed
     @fired_at = Gosu.milliseconds
   end
--- 06-physics/entities/components/bullet_physics.rb
+++ 06-physics/entities/components/bullet_physics.rb
@@ -4,8 +4,9 @@ class BulletPhysics < Component
   START_DIST = 20
   MAX_DIST = 300
 
-  def initialize(game_object)
-    super
+  def initialize(game_object, object_pool)
+    super(game_object)
+    @object_pool = object_pool
     object.x, object.y = point_at_distance(START_DIST)
     object.target_x, object.target_y = point_at_distance(MAX_DIST) if trajectory_lenght > MAX_DIST
   end
@@ -14,13 +15,12 @@ class BulletPhysics < Component
     fly_speed = Utils.adjust_speed(object.speed)
     fly_distance = (Gosu.milliseconds - object.fired_at) * 0.001 * fly_speed
     object.x, object.y = point_at_distance(fly_distance)
+    check_hit
     object.explode if arrived?
   end
 
   def trajectory_lenght
-    d_x = object.target_x - x
-    d_y = object.target_y - y
-    Math.sqrt(d_x * d_x + d_y * d_y)
+    Utils.distance_between(object.target_x, object.target_y, x, y)
   end
 
   def point_at_distance(distance)
@@ -33,6 +33,19 @@ class BulletPhysics < Component
   end
 
   private
+
+  def check_hit
+    # require 'pry'; binding.pry
+    @object_pool.nearby(object, 50).each do |obj|
+      next if obj == object.source # Don't hit source tank
+
+      next unless Utils.point_in_poly(x, y, *obj.box)
+
+      object.target_x = x
+      object.target_y = y
+      return
+    end
+  end
 
   def arrived?
     x == object.target_x && y == object.target_y
