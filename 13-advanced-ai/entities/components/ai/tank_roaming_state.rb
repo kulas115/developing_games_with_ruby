65a587a0cdec1d9017a6de1f7a7da2fc20868679
kulas115 <kulas115@gmail.com>
2022-04-15 12:57:18 +0200

    Make AI tanks seek powerups when roaming

--- 13-advanced-ai/entities/components/ai/tank_roaming_state.rb
+++ 13-advanced-ai/entities/components/ai/tank_roaming_state.rb
@@ -14,20 +14,45 @@ class TankRoamingState < TankMotionState
     end
   end
 
+  def required_powerups
+    required = []
+    health = @object.health.health
+    required << FireRatePowerup if @object.fire_rate_modifier < 2 && health > 50
+    required << TankSpeedPowerup if @object.speed_modifier < 1.5 && health > 50
+    required << RepairPowerup if health < 100
+    required << HealthPowerup if health < 190
+    required
+  end
+
   def change_direction
-    change = case rand(0..100)
-             when 0..30
-               -45
-             when 30..60
-               45
-             when 60..70
-               90
-             when 80..90
-               -90
-             else
-               0
-             end
-    @object.physics.change_direction(@object.direction + change) if change != 0
+    closest_powerup = @vision.closest_powerup(
+      *required_powerups
+    )
+    if closest_powerup
+      @seeking_powerup = true
+      angle = Utils.angle_between(
+        @object.x, @object.y,
+        closest_powerup.x, closest_powerup.y
+      )
+      @object.physics.change_direction(
+        angle - angle % 45
+      )
+    else
+      @seeking_powerup = false
+      change = case rand(0..100)
+               when 0..30
+                 -45
+               when 30..60
+                 45
+               when 60..70
+                 90
+               when 80..90
+                 -90
+               else
+                 0
+               end
+      @object.physics.change_direction(@object.direction + change) if change != 0
+    end
     @changed_direction_at = Gosu.milliseconds
     @will_keep_direction_for = turn_time
   end
@@ -41,6 +66,10 @@ class TankRoamingState < TankMotionState
   end
 
   def turn_time
-    rand(2000..5000)
+    if @seeking_powerup
+      rand(100..300)
+    else
+      rand(1000..3000)
+    end
   end
 end
